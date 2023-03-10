1d65c52755140e3a0aeee2d4a00134d6aa1fc8c9
Joseph Pire <josephpire.dev@gmail.com>
2023-03-10 07:33:20 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [sitemap(), prefetch(), tailwind(), image()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#1BA39C',
+					light: '#1B9E98',
+					dark: '#1F303D',
+				},
+				secondary: {
+					DEFAULT: '#1E454E',
+					light: '#1E454E',
+					dark: '#1E454E',
+				},
+			},
+			width: {
+				104: '26rem',
+				112: '28rem',
+				128: '32rem',
+			},
+		},
+	},
+	plugins: [],
+};
+
